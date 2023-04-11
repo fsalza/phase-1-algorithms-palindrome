9a1521f17e5684427ddfa350a3dcf2536100da78
Fulvio Salza <fsalza96@gmail.com>
2023-04-11 17:56:18 -0400

    complete

--- index.js
+++ index.js
@@ -1,10 +1,20 @@
 function isPalindrome(word) {
   // Write your algorithm here
+  let reversed = word.split("").reverse().join("");
+  return word === reversed
 }
 
-/* 
+isPalindrome("racecar")
+isPalindrome("robot")
+isPalindrome("chicken")
+isPalindrome("radar")
+
+/* if a word can be read the same backwards and forwards, return true. if not, return false.
   Add your pseudocode here
-*/
+// initialize a variable called word
+// if word is a isPalindrome, return true
+// if word is not a palindrome, retiurn false
+
 
 /*
   Add written explanation of your solution here
@@ -20,6 +30,12 @@ if (require.main === module) {
 
   console.log("Expecting: false");
   console.log("=>", isPalindrome("robot"));
+
+  console.log("Expecting: false");
+  console.log("=>", isPalindrome("chicken"))
+
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("radar"))
 }
 
 module.exports = isPalindrome;
